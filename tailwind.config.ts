60fee834c06d51dba4feeaef5b69e52ae2ff7e63
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-22 23:05:48 +0300

    Define Project colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./index.html",
         "./src/**/*.{ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "slate-gray": {
+                    "100": "#484949",
+                    "600": "#1c1d1c",
+                },
+                "burly-wood": "#d5b37e",
+                "gains-boro": "#e3e3e3",
+                "cod-gray": "#171617",
+                "dark-gray-slate": "#1b391d",
+                "dodger-blue": "#2780eb",
+                "midnight-blue": "#211f4f",
+                "royal-blue": "#225eff",
+                "medium-sea-green": "#17c259",
+                "crimson": "#e21543",
+                "maron": "#51081e"
+            }
+        },
     },
     plugins: [],
 }
