8d9de311388a7fd29b7027db5482329aa7c7404e
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-22 23:07:33 +0300

    Define constants and enums

---
+++ src/constants/index.ts
@@ -0,0 +1,28 @@
+import { GameChoice } from "@/enums";
+
+export const BET_AMOUNT: number = 500;
+export const MAX_CHOICE_BET: number = 2
+
+export const OUTCOMES: Record<GameChoice, GameChoice[]> = {
+    [GameChoice.Rock]: [ GameChoice.Scissors ],
+    [GameChoice.Paper]: [ GameChoice.Rock ],
+    [GameChoice.Scissors]: [ GameChoice.Paper ],
+};
+
+export const CHOICE_BUTTONS_COLORS = {
+    [GameChoice.Rock]: {
+        textColor: "text-dodger-blue",
+        backgroundColor: "bg-midnight-blue",
+        borderColor: "border-dodger-blue",
+    },
+    [GameChoice.Paper]: {
+        textColor: "text-medium-sea-green",
+        backgroundColor: "bg-dark-gray-slate",
+        borderColor: "border-medium-sea-green",
+    },
+    [GameChoice.Scissors]: {
+        textColor: "text-crimson",
+        backgroundColor: "bg-maron",
+        borderColor: "border-crimson",
+    }
+}
---
+++ src/enums/index.ts
@@ -0,0 +1,11 @@
+export enum GameChoice {
+    Rock = "rock",
+    Paper = "paper",
+    Scissors = "scissors",
+}
+
+export enum GameState {
+    Win = "win",
+    Lose = "lose",
+    Tie = "tie",
+}
