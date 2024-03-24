4ed55e2b7adc6fd701d9c2c4a1897840b1db95da
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-24 14:27:52 +0300

    Fix determine tie outcome logic

--- src/utils/determineOutcome.ts
+++ src/utils/determineOutcome.ts
@@ -3,10 +3,12 @@ import { GameChoice, GameOutcome } from "@/enums";
 
 export default function determineOutcome(playerChoices: GameChoice[], computerChoice: GameChoice): GameOutcome {
     if (playerChoices.includes(computerChoice)) {
-        return GameOutcome.TIE;
+        const isSingleBet: boolean = playerChoices.length == 1;
+
+        return isSingleBet ? GameOutcome.TIE : GameOutcome.LOSS;
     } else if (playerChoices.some(playerChoice => WIN_CONDITIONS[playerChoice].includes(computerChoice))) {
         return GameOutcome.WIN
-    } else {
-        return GameOutcome.LOSS
     }
+
+    return GameOutcome.LOSS;
 }
