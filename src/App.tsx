f86d17c5dc878ba56dab6c1737a02a6a76bbbd18
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-24 12:52:38 +0300

    Setup react-toastify and components and improve general page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { ToastContainer } from 'react-toastify';
+import { useGameStore } from "@/store";
+import Header from "@/components/Header.tsx";
+import Bets from "@/components/Bets.tsx";
+import ButtonAction from "@/components/ButtonAction.tsx";
+import BetOutcome from "@/components/BetOutcome.tsx";
+
 
 function App() {
+    const { isGameFinished } = useGameStore();
     return (
-        <div>
-        </div>
+        <main className="w-full h-full flex flex-col">
+            <Header/>
+
+            <section className="my-auto grid place-items-center gap-12">
+                    {isGameFinished ?  <BetOutcome/> : <div className="h-24"/>}
+
+                    <Bets/>
+                    <ButtonAction/>
+            </section>
+
+            <ToastContainer/>
+        </main>
     )
 }
 
